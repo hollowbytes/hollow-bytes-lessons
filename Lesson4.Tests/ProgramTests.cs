bb6a927462850a8462cc5602b764d733a44d7e37
shamonj03 <shamonj03@users.noreply.github.com>
2025-05-11 11:54:48 -0500

    Control flow

--- Lesson4.Tests/ProgramTests.cs
+++ Lesson4.Tests/ProgramTests.cs
@@ -1,9 +1,82 @@
-namespace Lesson3.Tests;
+using Shouldly;
+
+namespace Lesson4.Tests;
 
 public class ProgramTests
 {
     [Fact]
-    public void Test1()
+    public void GivenNoFirstNameIsEntered_ThenDisplayMissingFirstName()
     {
+        // Arrange
+        Console.SetIn(new StringReader(string.Empty));
+        
+        var output = new StringWriter();
+        Console.SetOut(output);
+        
+        var fakeArgs = Array.Empty<string>();
+
+        // Act
+        Program.Main(fakeArgs);
+        var actual = output.ToString();
+        
+        // Assert
+        actual.ShouldEndWith("Missing first name\r\n");
+    }
+    
+    [Fact]
+    public void GivenNoAgeIsEntered_ThenDisplayMissingAge()
+    {
+        // Arrange
+        Console.SetIn(new StringReader("test\n"));
+        
+        var output = new StringWriter();
+        Console.SetOut(output);
+        
+        var fakeArgs = Array.Empty<string>();
+
+        // Act
+        Program.Main(fakeArgs);
+        var actual = output.ToString();
+        
+        // Assert
+        // TODO: Use shouldly to test for missing age
+    }
+    
+    [Fact]
+    public void GivenAgeIsEntereed_WhenAgeIsBelow10_ThenDisplayTooYoung()
+    {
+        // Arrange
+        Console.SetIn(new StringReader("test\n8\n"));
+        
+        var output = new StringWriter();
+        Console.SetOut(output);
+        
+        var fakeArgs = Array.Empty<string>();
+
+        // Act
+        Program.Main(fakeArgs);
+        var actual = output.ToString();
+        
+        // Assert
+        // TODO: Use shouldly to test for too yung
+    }
+    
+    [Fact]
+    public void GivenCorrenctInformationIsEntered_ThenDisplayCongrats()
+    {
+        // Arrange
+        Console.SetIn(new StringReader("test\n30\n"));
+        
+        var output = new StringWriter();
+        Console.SetOut(output);
+        
+        var fakeArgs = Array.Empty<string>();
+
+        // Act
+        Program.Main(fakeArgs);
+        var actual = output.ToString();
+        
+        // Assert
+        // TODO: Use shouldly to test for congrats
     }
 }
--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,11 +1,24 @@
-﻿namespace Lesson2
+﻿namespace Lesson4
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            // TODO: Write 'Hello World' to the console
-            // When you run the test it should pass if done correctly
+            Console.Write("Enter your first name: ");
+            var firstName = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            if (int.TryParse(Console.ReadLine(), out var age))
+            {
+                age = 0;
+            }
+            
+            // TODO: Write some control logic where:
+            // TODO: If their first name is missing (BullOrEmpty) display 'Missing first name'
+            // TODO: Else if their age is missing (0) display 'Missing age'
+            // TODO: Else if their age is below 10 display 'Too young'
+            // TODO: Otherwise display 'Congrats!'
+            // TODO: Then go fill out the tests :)
         }
     }
 }
